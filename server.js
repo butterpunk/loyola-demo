66bcb0401da93519312cc50b97c46f14101b5fd2
butterpunk <blakebutterworth@gmail.com>
2017-07-12 10:24:13 -0500

    try again

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.listen(process.env.port || 5000, function(err) {  
+app.listen(process.env.PORT || 5000, function(err) {  
   if (err) {
     return console.log('something bad happened', err)
   }
 
 
-  console.log(`Magic is happening on ${port}`)
+  console.log(`Magic is happening on ${process.env.PORT}`)
 });
 
 
