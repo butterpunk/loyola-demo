8036538243d0c632477cb91d5dc31fe4d3f08ec2
butterpunk <blakebutterworth@gmail.com>
2017-07-12 10:11:24 -0500

    first comit

---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require('express')  
+const app = express()  
+const port = 3000
+var bodyParser = require('body-parser')
+var fs = require('fs');
+var mongoose = require('mongoose')
+
+Schema = new mongoose.Schema({
+	id : String,
+	description: String,
+	due_date: String
+},{ collection: 'todo' });
+
+
+
+mongoose.connect('mongodb://heroku_qgcjr9k0:dk9hse1csiu0l52rljicap4vsc@ds153392.mlab.com:53392/heroku_qgcjr9k0', function(error){
+	if (error) console.error(error);
+	else console.log('mongo connected');
+});
+
+mongoose.model('todo', Schema); 
+var Todo = mongoose.model('todo');
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.listen(port, function(err) {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+
+  console.log(`Magic is happening on ${port}`)
+});
+
+
+
+app.get('/todo-app', function(request, response) {  
+	Todo.find({},function(err,todo){
+		if(err){
+			console.log(err)
+		}else{
+			console.log(todo);
+		}
+	});
+});
+
+
+var allTodos = [{
+	"description" : "feed the dog",
+	"due_date" : "7/5/17",
+	"ID" : 1,
+},
+
+{
+	"description" : "wash the dishes",
+	"due_date" : "7/6/17",
+	"ID" : 2,
+},
+
+{
+	"description" : "take out the trash",
+	"due_date" : "7/7/17",
+	"ID" : 3,
+},]
+
+
+ 	
+
+
+app.get('/get-allTodos', function(request, response){
+	response.send(allTodos);
+	console.log('this route is being hit',allTodos);
+});
+
+app.post('/post-newTodos', function(request, response){
+	
+	
+	var newTodo = request.body;
+	allTodos.push(newTodo);
+	console.log(allTodos);
+
+});	
+
+ app.post('/post-edit', function(request, response){
+ 	
+ 	var requestData = request.body;
+ 	console.log(request.body);
+
+ 	for (var i=0; i<allTodos.length; i++){
+
+ 		console.log(allTodos[i].ID);
+ 		if(allTodos[i].ID==requestData.ID){
+ 			console.log('breaking into if statement');
+ 			allTodos[i].description = requestData.description;
+ 			console.log(allTodos[i]);
+ 			break;
+ 		}
+ 	}
+
+
+ });
+
+
+
+	// response.send(request.body);
+	// console.log('this route is being hit');
